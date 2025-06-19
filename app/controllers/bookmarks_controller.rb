479a13efe298ea90de4c0c2d695a221d1dc178bd
ROBIN DU APRC <ROBIN@PRIMES-SERVICES.BE>
2025-06-19 16:04:38 +0200

    copy-paste solution to catch back the lost time

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,5 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
   before_action :set_list, only: [:new, :create]
 
   def new
@@ -9,25 +10,29 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
-      redirect_to list_path(@list), notice: "Film ajouté à la liste."
+      redirect_to list_path(@list)
     else
+      @review = Review.new
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path(@bookmark.list), notice: "Bookmark supprimé."
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
 
-  def set_list
-    @list = List.find(params[:list_id])
-  end
-
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
