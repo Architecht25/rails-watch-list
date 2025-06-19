49d5c666cc4b37437c7918be299805eb117e0ec9
ROBIN DU APRC <ROBIN@PRIMES-SERVICES.BE>
2025-06-19 20:14:33 +0200

    end of copy/past to save time but need to study

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  has_many :reviews, dependant: :destroy
 
   validates :name, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_13_154919) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_19_180533) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -35,6 +35,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_13_154919) do
     t.text "overview"
     t.string "poster_url"
     t.float "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
+    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
